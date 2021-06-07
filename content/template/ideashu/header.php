d9c0a62bf9228c5d3186509044ff0e7fe8ff0379
amuke123 <32724294+amuke123@users.noreply.github.com>
2021-06-07 17:36:47 +0800

    logo

---
+++ content/template/ideashu/header.php
@@ -0,0 +1,5 @@
+<link rel="icon" sizes="any" href="<?php echo IDEA_URL . ADMIN_TYPE;?>/view/static/images/logo.svg" />
+<embed src="<?php echo IDEA_URL . ADMIN_TYPE;?>/view/static/images/logo.svg#icon1" type="image/svg+xml" />
+<svg>
+	<use xlink:href="<?php echo IDEA_URL . ADMIN_TYPE;?>/view/static/images/logo.svg#icon1"></use>
+</svg>
